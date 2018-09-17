79d18034b87a8c73e6ae27a3e8a6b1014c9ca6bf
Eliminater74 <eliminater74@gmail.com>
2018-09-17 11:29:22 -0400

    [LGE] [base] Import LG's base changes:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -28,6 +28,7 @@
 
 #include "base.h"
 #include "power/power.h"
+#include <soc/qcom/lge/board_lge.h>
 #if defined(CONFIG_MACH_LGE)
 #include <linux/timer.h>
 #include <linux/debugfs.h>
@@ -368,7 +369,13 @@ static int really_probe(struct device *dev, struct device_driver *drv)
 		if (nsec64_probe_spend)
 			bus_stime = ktime_get();
 #endif
+#if defined(CONFIG_LGE_PROBE_TIME_PROFILING) || defined(CONFIG_PROC_EVENTS)
+		dev_err(dev, "bus probe_log s\n");
+#endif
 		ret = dev->bus->probe(dev);
+#if defined(CONFIG_LGE_PROBE_TIME_PROFILING) || defined(CONFIG_PROC_EVENTS)
+		dev_err(dev, "bus probe_log e\n");
+#endif
 #if defined(CONFIG_MACH_LGE)
 		if (nsec64_probe_spend) {
 			int usecs, bus_us;
@@ -400,7 +407,13 @@ static int really_probe(struct device *dev, struct device_driver *drv)
 		if (nsec64_probe_spend)
 			drv_stime = ktime_get();
 #endif
+#if defined(CONFIG_LGE_PROBE_TIME_PROFILING) || defined(CONFIG_PROC_EVENTS)
+		dev_err(dev, "drv probe_log s\n");
+#endif
 		ret = drv->probe(dev);
+#if defined(CONFIG_LGE_PROBE_TIME_PROFILING) || defined(CONFIG_PROC_EVENTS)
+		dev_err(dev, "drv probe_log e\n");
+#endif
 #if defined(CONFIG_MACH_LGE)
 		if (nsec64_probe_spend) {
 			int usecs, drv_us;
@@ -450,10 +463,17 @@ probe_failed:
 			driver_deferred_probe_trigger();
 	} else if (ret != -ENODEV && ret != -ENXIO) {
 		/* driver matched but the probe failed */
+#if defined(CONFIG_PRE_SELF_DIAGNOSIS)
+        if(!strstr((char*)drv->name,"jtag") && !strstr((char*)drv->name,"coresight"))
+            lge_pre_self_diagnosis((char *) drv->bus->name,4,(char *) dev_name(dev),(char *) drv->name, ret);
+#endif
 		printk(KERN_WARNING
 		       "%s: probe of %s failed with error %d\n",
 		       drv->name, dev_name(dev), ret);
 	} else {
+#if defined(CONFIG_PRE_SELF_DIAGNOSIS)
+		lge_pre_self_diagnosis((char *) drv->bus->name,0,(char *) dev_name(dev),(char *) drv->name, ret);
+#endif
 		pr_debug("%s: probe of %s rejects match %d\n",
 		       drv->name, dev_name(dev), ret);
 	}
